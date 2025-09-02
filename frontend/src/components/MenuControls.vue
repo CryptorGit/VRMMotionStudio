5d051c96657056618c8230c5b1d9805d30f0f208
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-09-02 12:09:33 +0900

    メニューとレンダリング処理のコンポーネント分離

---
+++ frontend/src/components/MenuControls.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="menu" ref="menu">
+    <button id="menu-button" @click="toggleMenu"><i class="fa-solid fa-bars"></i></button>
+    <ul id="menu-list" :class="{ hidden: !menuOpen }">
+      <li id="import-option" @click="openFile"><i class="fa-solid fa-file-import"></i> インポート</li>
+      <li id="export-option" @click="exportPose"><i class="fa-solid fa-file-export"></i> エクスポート</li>
+      <li id="light-option" @click="openSidebarSection('lighting')"><i class="fa-solid fa-lightbulb"></i> ライト設定</li>
+      <li id="morph-option" @click="openSidebarSection('morph')"><i class="fa-solid fa-face-smile"></i> モーフ編集</li>
+      <li id="models-option" @click="openSidebarSection('models')"><i class="fa-solid fa-list"></i> モデル一覧</li>
+      <li id="clear-cache-option" @click="clearCache"><i class="fa-solid fa-trash"></i> キャッシュ削除</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  menuOpen: { type: Boolean, required: true },
+  toggleMenu: { type: Function, required: true },
+  openFile: { type: Function, required: true },
+  exportPose: { type: Function, required: true },
+  openSidebarSection: { type: Function, required: true },
+  clearCache: { type: Function, required: true }
+})
+
+const menu = ref(null)
+defineExpose({ menu })
+</script>
---
+++ frontend/src/composables/useRenderer.js
@@ -0,0 +1,8 @@
+import { useRenderLoop } from './useRenderLoop.js'
+import { useThreeViewerInit } from './useThreeViewerInit.js'
+
+export function useRenderer(ctx) {
+  const { animate, onWindowResize } = useRenderLoop(ctx)
+  const { init, cleanup } = useThreeViewerInit({ ...ctx, onWindowResize })
+  return { animate, initRenderer: init, cleanupRenderer: cleanup }
+}
